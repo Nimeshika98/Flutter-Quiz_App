246b38f86f986c4b4cda2611612e868f7bb7102c
Nimeshika98 <59416698+Nimeshika98@users.noreply.github.com>
2021-06-30 23:41:39 +0530

    fd4

--- QuizApp/lib/home.dart
+++ QuizApp/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/quizpage.dart';
 
 class homescreen extends StatefulWidget {
   //const homescreen({Key? key}) : super(key: key);
@@ -10,24 +11,23 @@ class _homescreenState extends State<homescreen> {
   List<String> images = [
     "images/py.png",
     "images/java.png",
-    "images/js.png",
     "images/cpp.png",
-    "images/linux.png",
+    "images/js.png",
+    //"images/linux.png",
   ];
 
-  Widget customcard() {
+  Widget customcard(String langname, String image) {
     return Padding(
       padding: EdgeInsets.symmetric(
         vertical: 20.0,
         horizontal: 30.0,
       ),
       child: InkWell(
-        // onTap: () {
-        //   Navigator.of(context).pushReplacement(MaterialPageRoute(
-
-        //     builder: (context) => getjson(langname),
-        //  )
-
+        onTap: () {
+          Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (context) => getjson(),
+          ));
+        },
         child: Material(
           color: Colors.pinkAccent,
           elevation: 10.0,
@@ -47,11 +47,36 @@ class _homescreenState extends State<homescreen> {
                         width: 200.0,
                         child: ClipOval(
                           child: Image(
-                            image: AssetImage("images/py.png"),
+                            fit: BoxFit.contain,
+                            image: AssetImage(
+                              image,
+                            ),
                           ),
                         ),
                       )),
                 ),
+                Center(
+                  child: Text(langname,
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        color: Colors.black,
+                        fontFamily: "Alike",
+                        fontWeight: FontWeight.w700,
+                      )),
+                ),
+                Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(
+                    "Let's Check Your basic Python knowledge",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.black,
+                      fontFamily: "Alike",
+                    ),
+                    maxLines: 5,
+                    textAlign: TextAlign.justify,
+                  ),
+                )
               ],
             ),
           ),
@@ -67,13 +92,17 @@ class _homescreenState extends State<homescreen> {
         title: Text(
           "Programz",
           style: TextStyle(
-            fontFamily: "Quando",
+            fontFamily: "Alike",
+            color: Colors.black,
           ),
         ),
       ),
       body: ListView(
         children: <Widget>[
-          customcard(),
+          customcard("Python", images[0]),
+          customcard("Java", images[1]),
+          customcard("C++", images[2]),
+          customcard("Javascript", images[3]),
         ],
       ),
     );
